157aa804aba06c5d6450641cc8b8e6d06cdd19d2
Yash Naik <yashnaik47@gmail.com>
2019-07-01 16:02:57 +0530

    Updates

---
+++ precache-manifest.752a2fc0925804e0e4a54536b1bf1a6b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cac2b40c39ce40708ffab8b8d7affe1",
+    "url": "/test8/index.html"
+  },
+  {
+    "revision": "b42aa7dd048f4b5d252a",
+    "url": "/test8/static/js/2.5c4e487f.chunk.js"
+  },
+  {
+    "revision": "bb77f9ed321e93b1bba9",
+    "url": "/test8/static/js/main.7c83e63a.chunk.js"
+  },
+  {
+    "revision": "a4cf49fec70e0968362b",
+    "url": "/test8/static/js/runtime~main.ded3ee31.js"
+  }
+]);
